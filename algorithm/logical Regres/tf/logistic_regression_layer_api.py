aba8d4f5cc25ca6adff1c6f145920c1debd62803
fengzhongyouxia <gzcl_wcon@126.com>
2018-06-03 13:30:07 +0800

    layer,slim api
    使用tensorflow layer api 于 tensorflow slim api

---
+++ algorithm/logical Regres/tf/logistic_regression_layer_api.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# mnist使用one_hot 编码
+'''
+A logistic regression learning algorithm example using TensorFlow library.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+# Parameters
+learning_rate = 0.05
+training_epochs = 25
+batch_size = 100
+display_step = 1
+
+# tf Graph Input
+x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
+y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+
+
+def xavier_init(n_inputs, n_outputs, uniform=True):
+  """Set the parameter initialization using the method described.
+  This method is designed to keep the scale of the gradients roughly the same
+  in all layers.
+  Xavier Glorot and Yoshua Bengio (2010):
+           Understanding the difficulty of training deep feedforward neural
+           networks. International conference on artificial intelligence and
+           statistics.
+  Args:
+    n_inputs: The number of input nodes into each output.
+    n_outputs: The number of output nodes for each input.
+    uniform: If true use a uniform distribution, otherwise use a normal.
+  Returns:
+    An initializer.
+  """
+  if uniform:
+    # 6 was used in the paper.
+    init_range = tf.sqrt(6.0 / (n_inputs + n_outputs))
+    return tf.random_uniform_initializer(-init_range, init_range)
+  else:
+    # 3 gives us approximately the same limits as above since this repicks
+    # values greater than 2 standard deviations from the mean.
+    stddev = tf.sqrt(3.0 / (n_inputs + n_outputs))
+    return tf.truncated_normal_initializer(stddev=stddev)
+
+# Set model weights
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+W=tf.get_variable('W',[784,10],initializer=xavier_init(784,10))
+b=tf.get_variable('b',[10],initializer=tf.random_uniform_initializer)
+
+# Construct model
+pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+# or
+# pred=tf.layers.dense(x,10,activation=tf.nn.softmax)
+# or
+# from tensorflow.contrib import slim
+# pred=slim.fully_connected(x,10,activation_fn=tf.nn.softmax)
+
+# Minimize error using cross entropy
+# cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
+# or
+cost=tf.losses.softmax_cross_entropy(onehot_labels=y,logits=pred)
+# Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
+                                                          y: batch_ys})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+
+    print("Optimization Finished!")
+
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
