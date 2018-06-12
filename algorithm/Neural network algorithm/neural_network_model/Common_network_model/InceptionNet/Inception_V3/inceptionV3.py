dd3d82d3381f96a335150b818b9e6f3ddb0e0685
fengzhongyouxia <gzcl_wcon@126.com>
2018-06-12 16:18:48 +0800

    inception net

--- algorithm/Neural network algorithm/neural_network_model/Common_network_model/InceptionNet/Inception_V3/inceptionV3.py
+++ algorithm/Neural network algorithm/neural_network_model/Common_network_model/InceptionNet/Inception_V3/inceptionV3.py
@@ -4,12 +4,12 @@ import tensorflow as tf
 # from tensorflow.contrib import slim
 
 
-x=tf.placeholder(tf.float32,[None,224,224,3],name='input_image')
+x=tf.placeholder(tf.float32,[None,299,299,3],name='input_image')
 num_classes=1000
 
 
 
-def inception_block(net,filters=[],scope='block_1',activation_fn=tf.nn.relu):
+def inception_block_1(net,filters=[],scope='block_1',activation_fn=tf.nn.relu):
     nets=[]
     with tf.variable_scope(scope):
         net1=tf.layers.conv2d(net,filters[0],1,1,'same',activation=activation_fn,name='conv1_1_1_s1')
@@ -43,41 +43,132 @@ def inception_block(net,filters=[],scope='block_1',activation_fn=tf.nn.relu):
 
     return net
 
+def inception_block_2(net,filters=[],scope='block_1',activation_fn=tf.nn.relu):
+    nets=[]
+    with tf.variable_scope(scope):
+        net1=tf.layers.conv2d(net,filters[0],1,1,'same',activation=activation_fn,name='conv1_1_1_s1')
+        net1=tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1=tf.layers.conv2d(net,filters[1],1,1,'same',activation=activation_fn,name='conv2_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        # net1=tf.layers.conv2d(net1,filters[2],3,1,'same',activation=activation_fn,name='conv2_3_3_s1')
+        net1 = tf.layers.conv2d(net1, filters[2], (1,7), 1, 'same', activation=activation_fn, name='conv2_1_7_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        net1 = tf.layers.conv2d(net1, filters[2], (7, 1), 1, 'same', activation=activation_fn, name='conv2_7_1_s1')
+
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1 = tf.layers.conv2d(net, filters[3], 1, 1, 'same', activation=activation_fn, name='conv3_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        # V1
+        # net1 = tf.layers.conv2d(net1, filters[4], 5, 1, 'same', activation=activation_fn, name='conv3_5_5_s1')
+        # V2
+        # net1 = tf.layers.conv2d(net1, filters[4],3, 1, 'same', activation=activation_fn, name='conv3_3_3_s1_0')
+        net1 = tf.layers.conv2d(net1, filters[4], (1,7), 1, 'same', activation=activation_fn, name='conv3_1_7_s1_0')
+        net1 = tf.layers.batch_normalization(net1)
+        net1 = tf.layers.conv2d(net1, filters[4], (7, 1), 1, 'same', activation=activation_fn, name='conv3_7_1_s1_0')
+
+        net1 = tf.layers.batch_normalization(net1)
+        # net1 = tf.layers.conv2d(net1, filters[4], 3, 1, 'same', activation=activation_fn, name='conv3_3_3_s1_1')
+        net1 = tf.layers.conv2d(net1, filters[4], (1, 7), 1, 'same', activation=activation_fn, name='conv3_1_7_s1_1')
+        net1 = tf.layers.batch_normalization(net1)
+        net1 = tf.layers.conv2d(net1, filters[4], (7, 1), 1, 'same', activation=activation_fn, name='conv3_7_1_s1_1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1=tf.layers.max_pooling2d(net,3,1,'same',name='pool4_3_3_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        net1=tf.layers.conv2d(net1,filters[5],1,1,'same',activation=activation_fn,name='conv4_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net=tf.concat(nets,-1) # [28,28,256]
+
+    return net
+
+def inception_block_3(net,filters=[],scope='block_1',activation_fn=tf.nn.relu):
+    nets=[]
+    with tf.variable_scope(scope):
+        net1=tf.layers.conv2d(net,filters[0],1,1,'same',activation=activation_fn,name='conv1_1_1_s1')
+        net1=tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1=tf.layers.conv2d(net,filters[1],1,1,'same',activation=activation_fn,name='conv2_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        net1=tf.layers.conv2d(net1,filters[2],(1,3),1,'same',activation=activation_fn,name='conv2_1_3_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+        net1 = tf.layers.conv2d(net1, filters[2], (3,1), 1, 'same', activation=activation_fn, name='conv2_3_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1 = tf.layers.conv2d(net, filters[3], 1, 1, 'same', activation=activation_fn, name='conv3_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        # V1
+        # net1 = tf.layers.conv2d(net1, filters[4], 5, 1, 'same', activation=activation_fn, name='conv3_5_5_s1')
+        # V2
+        net1 = tf.layers.conv2d(net1, filters[4],3, 1, 'same', activation=activation_fn, name='conv3_3_3_s1_0')
+        net1 = tf.layers.batch_normalization(net1)
+        net1 = tf.layers.conv2d(net1, filters[4], (1,3), 1, 'same', activation=activation_fn, name='conv3_1_3_s1_1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+        net1 = tf.layers.conv2d(net1, filters[4], (3,1), 1, 'same', activation=activation_fn, name='conv3_3_1_s1_1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net1=tf.layers.max_pooling2d(net,3,1,'same',name='pool4_3_3_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        net1=tf.layers.conv2d(net1,filters[5],1,1,'same',activation=activation_fn,name='conv4_1_1_s1')
+        net1 = tf.layers.batch_normalization(net1)
+        nets.append(net1)
+
+        net=tf.concat(nets,-1) # [28,28,256]
+
+    return net
 
 
-def Inception_V2(x,softmax0=False,softmax1=False):
+
+def Inception_V3(x,softmax0=False,softmax1=False):
     net=x
     net = tf.layers.batch_normalization(net)
-    net=tf.layers.conv2d(net,64,7,2,'same',activation=tf.nn.relu,name='conv1_7_7_s2') # [112,112,64]
+    net=tf.layers.conv2d(net,32,3,2,'valid',activation=tf.nn.relu,name='conv1_3_3_v2_0') # [149,149,32]
     net = tf.layers.batch_normalization(net)
-    net=tf.layers.max_pooling2d(net,3,2,'same',name='pool1_3_3_s2') # [56,56,64]
+    net = tf.layers.conv2d(net, 32, 3, 1, 'valid', activation=tf.nn.relu, name='conv1_3_3_v2_1')  # [147,147,32]
+    net = tf.layers.batch_normalization(net)
+    net = tf.layers.conv2d(net, 64, 3, 1, 'same', activation=tf.nn.relu, name='conv1_3_3_s2_2')  # [147,147,64]
+    net = tf.layers.batch_normalization(net)
+    net=tf.layers.max_pooling2d(net,3,2,'valid',name='pool1_3_3_v2') # [73,73,64]
     # net=tf.nn.local_response_normalization(net)
     net = tf.layers.batch_normalization(net)
 
-    net=tf.layers.conv2d(net,64,1,1,'valid',activation=tf.nn.relu,name='conv2_1_1_v1')# [56,56,64]
+    net=tf.layers.conv2d(net,80,3,1,'valid',activation=tf.nn.relu,name='conv2_3_3_v1_0') # [71,71,80]
     net = tf.layers.batch_normalization(net)
-    net = tf.layers.conv2d(net, 192, 3, 1, 'same', activation=tf.nn.relu, name='conv2_3_3_s1')  # [56,56,192]
+    net = tf.layers.conv2d(net, 192, 3, 2, 'valid', activation=tf.nn.relu, name='conv2_3_3_v2_1')  # [35,35,192]
     net = tf.layers.batch_normalization(net)
-    # net = tf.nn.local_response_normalization(net)
-    net = tf.layers.max_pooling2d(net, 3, 2, 'same', name='pool2_3_3_s2') # [28,28,192]
+    net = tf.layers.conv2d(net, 288, 3, 1, 'same', activation=tf.nn.relu, name='conv2_3_3_s1_2')  # [35,35,288]
     net = tf.layers.batch_normalization(net)
 
-    filters=[64,96,128,16,32,32]
-    net=inception_block(net,filters,'block_3a') # [28,28,256]
-    net = tf.layers.batch_normalization(net)
     filters = [128, 128, 192, 32, 96, 64]
-    net=inception_block(net,filters,'block_3b') # [28,28,480]
+    net=inception_block_1(net,filters,'block_3a') # [35,35,480]
+    net = tf.layers.batch_normalization(net)
+    filters = [160, 128, 224, 64, 128, 96]
+    net = inception_block_1(net, filters, 'block_3b')  # [35,35,608]
+    net = tf.layers.batch_normalization(net)
+    filters = [192, 128, 256, 96, 160, 160]
+    net = inception_block_1(net, filters, 'block_3c')  # [35,35,768]
     net = tf.layers.batch_normalization(net)
 
-    net=tf.layers.max_pooling2d(net,3,2,'same',name='pool3_3_3_s2') # [14,14,480]
+    net=tf.layers.max_pooling2d(net,3,2,'valid',name='pool3_3_3_v2') # [17,17,768]
     net = tf.layers.batch_normalization(net)
 
-    filters = [192, 96, 208, 16, 48, 64]
-    net = inception_block(net, filters, 'block_4a')  # [14,14,512]
-    net = tf.layers.batch_normalization(net)
 
+    filters = [224, 128, 288, 96, 192, 160]
+    net = inception_block_2(net, filters, 'block_4a')  # [17,17,864]
+    net = tf.layers.batch_normalization(net)
     if softmax0:
-        net1=tf.layers.average_pooling2d(net,5,3,'valid',name='softmax0_pool_5_5_v3') # [4,4,512]
+        net1=tf.layers.average_pooling2d(net,17,1,'valid',name='softmax0_pool_17_17_v3') # [1,1,864]
         net1 = tf.layers.batch_normalization(net1)
         net1=tf.layers.conv2d(net1,1024,1,1,'same',activation=tf.nn.relu,name='softmax0_conv_1_1_s1') # [4,4,1024]
         net1 = tf.layers.batch_normalization(net1)
@@ -87,18 +178,18 @@ def Inception_V2(x,softmax0=False,softmax1=False):
         net1 = tf.layers.batch_normalization(net1)
         net1 = tf.layers.dense(net1, num_classes, activation=tf.nn.softmax, name='softmax0_output_1')  # [-1,1000]
         return net1
+    filters = [256, 160, 320, 128, 192, 192]
+    net = inception_block_2(net, filters, 'block_4b')  # [17,17,960]
+    net = tf.layers.batch_normalization(net)
+    filters = [288, 160, 352, 128, 224, 224]
+    net = inception_block_2(net, filters, 'block_4c')  # [17,17,1088]
+    net = tf.layers.batch_normalization(net)
+    filters = [320, 192, 384, 128, 256, 256]
+    net = inception_block_2(net, filters, 'block_4d')  # [17,17,1216]
+    net = tf.layers.batch_normalization(net)
 
-    filters = [160, 112, 224, 24, 64, 64]
-    net = inception_block(net, filters, 'block_4b')  # [14,14,512]
-    net = tf.layers.batch_normalization(net)
-    filters = [128, 128, 256, 24, 64, 64]
-    net = inception_block(net, filters, 'block_4c')  # [14,14,512]
-    net = tf.layers.batch_normalization(net)
-    filters = [112, 144, 288, 32, 64, 64]
-    net = inception_block(net, filters, 'block_4d')  # [14,14,528]
-    net = tf.layers.batch_normalization(net)
     if softmax1:
-        net1=tf.layers.average_pooling2d(net,5,3,'valid',name='softmax1_pool_5_5_v3') # [4,4,528]
+        net1=tf.layers.average_pooling2d(net,17,1,'valid',name='softmax1_pool_17_17_v3') # [1,1,1216]
         net1 = tf.layers.batch_normalization(net1)
         net1=tf.layers.conv2d(net1,1024,1,1,'same',activation=tf.nn.relu,name='softmax1_conv_1_1_s1') # [4,4,1024]
         net1 = tf.layers.batch_normalization(net1)
@@ -108,22 +199,23 @@ def Inception_V2(x,softmax0=False,softmax1=False):
         net1 = tf.layers.batch_normalization(net1)
         net1 = tf.layers.dense(net1, num_classes, activation=tf.nn.softmax, name='softmax1_output_1') # [-1,1000]
         return net1
-
-    filters = [256, 160, 320, 32, 128, 128]
-    net = inception_block(net, filters, 'block_4e')  # [14,14,832]
-    net = tf.layers.batch_normalization(net)
-    net = tf.layers.max_pooling2d(net, 3, 2, 'same', name='pool4_3_3_s2')  # [7,7,832]
+    filters = [352, 208, 416, 128, 256, 256]
+    net = inception_block_2(net, filters, 'block_4e')  # [17,17,1280]
     net = tf.layers.batch_normalization(net)
 
-    filters = [256, 160, 320, 32, 128, 128]
-    net = inception_block(net, filters, 'block_5a')  # [7,7,832]
+    net = tf.layers.max_pooling2d(net, 3, 2, 'valid', name='pool4_3_3_v2')  # [8,8,1280]
     net = tf.layers.batch_normalization(net)
 
-    filters = [384, 192, 384, 48, 128, 128]
-    net = inception_block(net, filters, 'block_5b')  # [7,7,1024]
+    filters = [352, 208, 416, 128, 256, 256]
+    net = inception_block_3(net, filters, 'block_5a')  # [8,8,1952]
     net = tf.layers.batch_normalization(net)
 
-    net=tf.layers.average_pooling2d(net,7,1,'valid',name='pool5_7_7_v1') # [1,1,1024]
+    filters = [352, 208, 448, 128, 256, 288]
+    net = inception_block_3(net, filters, 'block_5b')  # [8,8,2048]
+    net = tf.layers.batch_normalization(net)
+
+    net=tf.layers.average_pooling2d(net,8,1,'valid',name='ave_pool_8_8_v1') # [1,1,2048]
+
     net = tf.layers.batch_normalization(net)
     net=tf.layers.dropout(net,0.4)
     net=tf.layers.flatten(net)
@@ -133,6 +225,6 @@ def Inception_V2(x,softmax0=False,softmax1=False):
     return net
 
 
-pred=Inception_V2(x,True)
+pred=Inception_V3(x)
 
 print(pred.shape)
